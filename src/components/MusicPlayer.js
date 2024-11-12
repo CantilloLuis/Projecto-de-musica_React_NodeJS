88d16e7bf4ba453786ca075e9e11540fabdceeef
Luis Eduardo Cantillo Zapateiro <cantilloluiseduardo52@gmail.com>
2024-11-12 10:15:59 -0500

    Agregando la logica del promedio de calificacion de cada musica, se evidencia la puntuacion en el reproductor de musica

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -11,12 +11,32 @@ function MusicPlayer() {
     const [likes, setLikes] = useState(0); // Start with likes as 0 initially
     const [liked, setLiked] = useState(false); // State to track if the song has likes
     const [isPlaying, setIsPlaying] = useState(false); // Track if the music is playing
+    const [ratingAverage, setRatingAverage] = useState(0);
+
 
     useEffect(() => {
         if (currentSong) {
             setAudioSrc(`http://localhost:3001/api/music/audio/${currentSong.fileId}/`);
             setLikes(currentSong.likes || 0); // Safely access likes, fallback to 0 if undefined
             setLiked(currentSong.likes > 0);
+
+            // Calculate the average rating from comments with 'calificacion' field
+            if (currentSong.comentarios && currentSong.comentarios.length > 0) {
+                const ratings = currentSong.comentarios
+                    .map(comentario => comentario.calificacion)
+                    .filter(calificacion => calificacion !== undefined && calificacion !== 0); // Ensure calificacion exists
+
+                if (ratings.length > 0) {
+                    const totalRating = ratings.reduce((acc, calificacion) => acc + calificacion, 0);
+                    const averageRating = totalRating / ratings.length;
+                    setRatingAverage(averageRating);
+                } else {
+                    setRatingAverage(0);
+                }
+            } else {
+                setRatingAverage(0);
+            }
+
         }
     }, [currentSong]);
 
@@ -69,6 +89,8 @@ function MusicPlayer() {
                         <p>Best of 2024</p>
                         <p>{Math.floor(progress / 60)}:{Math.floor(progress % 60).toString().padStart(2, '0')}</p>
                         <h3>Duración: {currentSong.duracion}</h3>
+                        <h4>Calificación general: {ratingAverage}</h4>
+
                         {isPlaying && (
                             <div className="visualizer">
                                 <img src="https://cdn.pixabay.com/animation/2023/10/10/13/26/13-26-45-476_512.gif" alt="Visualizer" />
